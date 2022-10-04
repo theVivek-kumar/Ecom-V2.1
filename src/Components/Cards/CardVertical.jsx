92bf8e0b0a000272275eff036a583cf9487aa8ff
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-10-04 23:47:04 +0530

    fix-ui

--- src/Components/Cards/CardVertical.jsx
+++ src/Components/Cards/CardVertical.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Products from '../Products/Products'
 function CardVertical({title,img,desc}) {
    console.log(title,desc)
    
@@ -14,6 +14,7 @@ function CardVertical({title,img,desc}) {
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
         </a>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis voluptate, perspiciatis recusandae ad, dolorum sint deleniti, sunt expedita totam tenetur similique modi quis. Tenetur accusamus voluptas numquam architecto hic quaerat.</p>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
         <div class="flex items-center">
     <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>

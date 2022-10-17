1d379acd7e77e2ee7419400d2c6d339f6ca9f1df
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-10-17 23:37:40 +0530

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </a>
         <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
             </li>
             <li>
                 <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
